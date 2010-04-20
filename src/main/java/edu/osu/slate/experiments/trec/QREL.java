e01e39f95a4defd440a4b092cfa771dff4ca7647
timothy.weale <timothy.weale@52d54d0c-1504-25e0-5e28-ad71def8b7ec>
2010-04-20 01:33:06 +0000

    Update TREC classes

--- src/main/java/edu/osu/slate/experiments/trec/QREL.java
+++ src/main/java/edu/osu/slate/experiments/trec/QREL.java
@@ -4,23 +4,23 @@ import java.util.*;
 
 public class QREL {
 
-	private LinkedList<String> validDocs;
-	private int queryNum;
+  private LinkedList<String> validDocs;
+  private int queryNum;
 	
-	public QREL(int num) {
-		validDocs = new LinkedList<String>();
-		queryNum = num;
-	}
+  public QREL(int num) {
+    validDocs = new LinkedList<String>();
+    queryNum = num;
+  }
 	
-	public void addValidDoc(String s) {
-		validDocs.add(s);
-	}
+  public void addValidDoc(String s) {
+    validDocs.add(s);
+  }
 	
-	public LinkedList<String> getValidDocs() {
-		return validDocs;
-	}
+  public LinkedList<String> getValidDocs() {
+    return validDocs;
+  }
 	
-	public int getQueryNum() {
-		return queryNum;
-	}
+  public int getQueryNum() {
+    return queryNum;
+  }
 }
--- src/main/java/edu/osu/slate/experiments/trec/QRELSFile.java
+++ src/main/java/edu/osu/slate/experiments/trec/QRELSFile.java
@@ -5,46 +5,46 @@ import java.util.*;
 
 public class QRELSFile {
 
-	private LinkedList<QREL> answers;
+  private LinkedList<QREL> answers;
 	
-	private Iterator<QREL> it;
+  private Iterator<QREL> it;
 	
-	public QRELSFile(String filename) throws IOException {
-		
-		answers = new LinkedList<QREL>();
-		Scanner fin = new Scanner(new FileReader(filename));
+  public QRELSFile(String filename) throws IOException {
+    
+    answers = new LinkedList<QREL>();
+    Scanner fin = new Scanner(new FileReader(filename));
 
-		int currNum = -1;
-		QREL lq = null;
-		while(fin.hasNext()) {
-			String line = fin.nextLine();
-			String [] arr = line.split(" ");
+    int currNum = -1;
+    QREL lq = null;
+    while(fin.hasNext()) {
+      String line = fin.nextLine();
+      String [] arr = line.split(" ");
+      
+      if(currNum != Integer.parseInt(arr[0])) {
+				
+        currNum = Integer.parseInt(arr[0]);
+				
+        if(lq != null) {
+          answers.add(lq);
+        }
+				
+        lq = new QREL(currNum);
+      }
 			
-			if(currNum != Integer.parseInt(arr[0])) {
-				
-				currNum = Integer.parseInt(arr[0]);
-				
-				if(lq != null) {
-					answers.add(lq);
-				}
-				
-				lq = new QREL(currNum);
-			}
-			
-			if(arr[3].equals("1")) {
-				lq.addValidDoc(arr[2]);
-			}
-		}
+      if(arr[3].equals("1")) {
+        lq.addValidDoc(arr[2]);
+      }
+    }
 
-		answers.add(lq);
-		it = answers.iterator();
-	}
+    answers.add(lq);
+    it = answers.iterator();
+  }
 	
-	public boolean hasNext() {
-		return it.hasNext();
-	}
+  public boolean hasNext() {
+    return it.hasNext();
+  }
 	
-	public QREL next() {
-		return it.next();
-	}
+  public QREL next() {
+    return it.next();
+  }
 }
