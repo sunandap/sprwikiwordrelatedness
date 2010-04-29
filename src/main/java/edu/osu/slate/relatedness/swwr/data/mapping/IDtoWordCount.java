96101b692c690b58a75310cd0ab63302679f293f
timothy.weale <timothy.weale@52d54d0c-1504-25e0-5e28-ad71def8b7ec>
2010-04-29 16:31:58 +0000

    Added mapping classes

---
+++ src/main/java/edu/osu/slate/relatedness/swwr/data/mapping/IDtoWordCount.java
@@ -0,0 +1,137 @@
+/* Copyright 2010 Speech and Language Technologies Lab, The Ohio State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.osu.slate.relatedness.swwr.data.mapping;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+
+
+/**
+ * Class used to Map a given ID to a set of words.
+ * <p>
+ * Uses an array of {@link WordCount} classes for the mapping.
+ * 
+ * @author weale
+ *
+ */
+public class IDtoWordCount implements Serializable {
+  
+ /**
+   * 
+   */
+  private static final long serialVersionUID = 7700956091903015001L;
+
+ /**
+  * Pairs of (Word, Count) Mappings
+  */
+  private WordCount[] wordcounts;
+  
+ /**
+  * ID for Mapping 
+  */
+  private int id;
+  
+ /**
+  * Constructor.
+  *  
+  * @param i ID to Map to {@link WordCount} pairs
+  */
+  public IDtoWordCount(int i) {
+    id = i;
+    wordcounts = new WordCount[0];
+  }
+  
+ /**
+  * Gets the ID for this mapping.
+  * 
+  * @return ID contained in this mapping
+  */
+  public int getID() {
+    return id;
+  }
+  
+ /**
+  * Gets the array of {@link WordCount} pairs for the given word.
+  * 
+  * @return {@link WordCount} array contained in this mapping
+  */
+  public WordCount[] getIDCounts() {
+    return wordcounts;
+  }
+  
+ /**
+  * Adds a new ID for the given word.
+  * 
+  * If the ID is found, we increment the existing count.
+  * If the ID is not found, we extend the given {@link IDCount} array.
+  * 
+  * @param id New ID for the given word
+  */
+  public void addWord(String w) {
+    int pos = Arrays.binarySearch(wordcounts, new WordCount(w,1));
+    if(pos >= 0)
+    {
+      //IDCount found!
+      int count = wordcounts[pos].getCount();
+      wordcounts[pos].setCount(count+1);
+    }
+    else
+    {
+      //Add new IDCount
+      WordCount[] tmp = new WordCount[wordcounts.length];
+      System.arraycopy(wordcounts, 0, tmp, 0, wordcounts.length);
+      tmp[wordcounts.length] = new WordCount(w, 1);
+      wordcounts = tmp;
+    }
+  }//end: addID
+  
+ /**
+  * 
+  * @param out
+  * @throws IOException
+  */
+  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+    
+    // Write String
+    out.writeInt(id);
+    
+    // Write array of IDCounts
+    out.writeInt(wordcounts.length);
+    for(int i=0; i<wordcounts.length; i++) {
+      out.writeObject(wordcounts[i]);
+    }
+  }
+  
+ /**
+  *  
+  * @param in
+  * @throws IOException
+  * @throws ClassNotFoundException
+  */
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    
+    // Read String
+    id = in.readInt();
+    
+    // Read array of IDCounts
+    int len = in.readInt();
+    wordcounts = new WordCount[len];
+    for(int i=0; i<len; i++) {
+      wordcounts[i] = (WordCount) in.readObject();
+    }//end: for(i)
+  }//end: readObject
+}
---
+++ src/main/java/edu/osu/slate/relatedness/swwr/data/mapping/WordToIDCount.java
@@ -0,0 +1,137 @@
+/* Copyright 2010 Speech and Language Technologies Lab, The Ohio State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.osu.slate.relatedness.swwr.data.mapping;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+
+
+/**
+ * Class to Map a given word to a set of IDs.
+ * <p>
+ * Uses an array of {@link IDCount} classes for the mapping.
+ * 
+ * @author weale
+ *
+ */
+public class WordToIDCount implements Serializable {
+  
+ /**
+   * 
+   */
+  private static final long serialVersionUID = 7700956091903015001L;
+
+ /**
+  * Pairs of (Vertex, Count) Mappings
+  */
+  private IDCount[] idcounts;
+  
+ /**
+  * Word for Mapping 
+  */
+  private String word;
+  
+ /**
+  * Constructor.
+  *  
+  * @param w Word to Map to {@link IDCount} pairs
+  */
+  public WordToIDCount(String w) {
+    word = w;
+    idcounts = new IDCount[0];
+  }
+  
+ /**
+  * Gets the Word for this mapping.
+  * 
+  * @return Word contained in this mapping
+  */
+  public String getWord() {
+    return word;
+  }
+  
+ /**
+  * Gets the array of {@link IDCount} pairs for the given word.
+  * 
+  * @return {@link IDCount} array contained in this mapping
+  */
+  public IDCount[] getIDCounts() {
+    return idcounts;
+  }
+  
+ /**
+  * Adds a new ID for the given word.
+  * 
+  * If the ID is found, we increment the existing count.
+  * If the ID is not found, we extend the given {@link IDCount} array.
+  * 
+  * @param id New ID for the given word
+  */
+  public void addID(int id) {
+    int pos = Arrays.binarySearch(idcounts, new IDCount(id,1));
+    if(pos >= 0)
+    {
+      //IDCount found!
+      int count = idcounts[pos].getCount();
+      idcounts[pos].setCount(count+1);
+    }
+    else
+    {
+      //Add new IDCount
+      IDCount[] tmp = new IDCount[idcounts.length];
+      System.arraycopy(idcounts, 0, tmp, 0, idcounts.length);
+      tmp[idcounts.length] = new IDCount(id, 1);
+      idcounts = tmp;
+    }
+  }//end: addID
+  
+ /**
+  * 
+  * @param out
+  * @throws IOException
+  */
+  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+    
+    // Write String
+    out.writeObject(word);
+    
+    // Write array of IDCounts
+    out.writeInt(idcounts.length);
+    for(int i=0; i<idcounts.length; i++) {
+      out.writeObject(idcounts[i]);
+    }
+  }
+  
+ /**
+  *  
+  * @param in
+  * @throws IOException
+  * @throws ClassNotFoundException
+  */
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    
+    // Read String
+    word = (String) in.readObject();
+    
+    // Read array of IDCounts
+    int len = in.readInt();
+    idcounts = new IDCount[len];
+    for(int i=0; i<len; i++) {
+      idcounts[i] = (IDCount) in.readObject();
+    }//end: for(i)
+  }//end: readObject
+}
