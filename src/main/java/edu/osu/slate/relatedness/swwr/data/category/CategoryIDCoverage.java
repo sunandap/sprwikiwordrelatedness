1c67fc417e12d18fef0cd20f014a2d71d6e37d11
timothy.weale <timothy.weale@52d54d0c-1504-25e0-5e28-ad71def8b7ec>
2010-05-01 20:18:57 +0000

    Added category data classes.

---
+++ src/main/java/edu/osu/slate/relatedness/swwr/data/category/CategoryIDCoverage.java
@@ -0,0 +1,68 @@
+/* Copyright 2010 Speech and Language Technologies Lab, The Ohio State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.osu.slate.relatedness.swwr.data.category;
+
+/**
+ * Class containing a (Category ID, coverage) pair.
+ * 
+ * @author weale
+ * @version 1.0
+ */
+public class CategoryIDCoverage {
+
+  private int catID;
+  private float coverage;
+  
+ /**
+  * Constructor.
+  *  
+  * @param id Category ID.
+  * @param cov Category coverage.
+  */
+  public CategoryIDCoverage(int id, float cov) {
+    catID = id;
+    coverage = cov;
+  }//end: CategoryIDCoverage(int,float)
+  
+ /**
+  * Sets the category coverage.
+  * 
+  * @param c New coverage amount.
+  */
+  public void setCoverage(float c)
+  {
+    coverage = c;
+  }//end: setCoverage()
+  
+ /**
+  * Returns the coverage value for the (ID, coverage) pair.
+  * 
+  * @return Coverage value.
+  */
+  public float getCoverage()
+  {
+    return coverage;
+  }//end: getCoverage()
+  
+ /**
+  * Returns the Category ID for the (ID, coverage) pair.
+  * 
+  * @return Category ID.
+  */
+  public int getCatID() {
+    return catID;
+  }//end: getCatID()
+}
